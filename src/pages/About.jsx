19a7c5910bca5bdb317721755b1267bbbbda3bac
Vladislav Burhovetskiy <mylivladislav@gmail.com>
2023-08-22 15:12:15 +0300

    Changed path as assets

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import bgImg from "/public/images/about-hero.png";
+import bgImg from "/assets/images/about-hero.png";
 import { Link } from "react-router-dom";
 
 export default function About() {
